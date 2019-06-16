d7fa694a8f52d5ee80bf97e937ac69e5f2273c00
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-15 22:46:20 -0400

    CelsiusToFahrenheitConversion - completed

--- CelsiusToFahrenheitConversion/CelsiusToFahrenheitConversion/Program.cs
+++ CelsiusToFahrenheitConversion/CelsiusToFahrenheitConversion/Program.cs
@@ -16,6 +16,8 @@ namespace CelsiusToFahrenheitConversion
             celsius = int.Parse(Console.ReadLine());
             // Calculate temperature in fahrenheit from celsius
             faren = (celsius * 9) / 5 + 32;
+            Console.WriteLine("Temperature in Fahrenheit: {0}", faren);
+            Console.ReadLine();
         }
     }
 }
