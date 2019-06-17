0555c880ced1063a8be0de849f3f90fad7afceb6
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-16 21:51:04 -0400

    Implemenation of Switch statements and 1st case

--- CurrencyConversion/CurrencyConversion/Program.cs
+++ CurrencyConversion/CurrencyConversion/Program.cs
@@ -14,6 +14,19 @@ namespace CurrencyConversion
             int choice;
             Console.WriteLine("Enter your choice : \n 1-Dollar to Rupee \n 2-Euro to Rupee \n 3- Ringgit to Rupee");
             choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Double dollar, rupee, val;
+                    Console.WriteLine("Enter the Dollar Amount: ");
+                    dollar = Double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the Dollar Value : ");
+                    val = double.Parse(Console.ReadLine());
+                    rupee = dollar * val;
+                    Console.WriteLine("{0} Dollar equals {1} Rupees", dollar, rupee);
+                    break;
+            }
         }
     }
 }
