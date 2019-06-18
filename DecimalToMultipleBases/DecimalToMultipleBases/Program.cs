65c1bf9d30b34e425652416b007cc1f21c72f0ff
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-17 21:29:12 -0400

    DecimalToMultipleBases conversions - initial commit

---
+++ DecimalToMultipleBases/DecimalToMultipleBases/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalToMultipleBases
+{
+    // Program to convert Decimal to Multiple base using Stack
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
