2073a210cc8ee1051b6e3228c8bea6171cecdac8
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-18 20:33:19 -0400

    Star Pattern 2

--- StarPatternsProgram/StarPatternsProgram/Program.cs
+++ StarPatternsProgram/StarPatternsProgram/Program.cs
@@ -22,6 +22,16 @@ namespace StarPatternsProgram
                 Console.WriteLine();
             }
 
+            // Pattern 2
+            for(int row=1; row <=8; ++row)
+            {
+                for(int col=1; col <=row; ++col)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
