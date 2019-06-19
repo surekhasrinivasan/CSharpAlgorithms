6e7d563a640dc538f288d251d3c2a7999568c496
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-18 20:22:25 -0400

    StarPatternsProgram - initial commit

---
+++ StarPatternsProgram/StarPatternsProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarPatternsProgram
+{
+    // Program to demo different Star Patterns 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
