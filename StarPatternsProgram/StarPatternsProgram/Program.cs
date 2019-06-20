f33662799229f926dd439dc5378cdfc20b9c20e5
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-20 18:49:17 -0400

    Star Pattern-6 Takes user input

--- StarPatternsProgram/StarPatternsProgram/Program.cs
+++ StarPatternsProgram/StarPatternsProgram/Program.cs
@@ -11,100 +11,108 @@ namespace StarPatternsProgram
     {
         static void Main(string[] args)
         {
-            // Pattern 1
+            //// Pattern 1
 
-            for(int row = 8; row >=1; --row)
-            {
-                for(int col=1;col <= row; ++col)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
-            }
+            //for(int row = 8; row >=1; --row)
+            //{
+            //    for(int col=1;col <= row; ++col)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
 
-            // Pattern 2
-            for(int row=1; row <=8; ++row)
-            {
-                for(int col=1; col <=row; ++col)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
-            }
+            //// Pattern 2
+            //for(int row=1; row <=8; ++row)
+            //{
+            //    for(int col=1; col <=row; ++col)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
 
-            // Pattern 3
+            //// Pattern 3
 
-            int val = 8;
-            int i, j, k;
-            for(i = 1; i <=val; i++)
-            {
-                for (j = 1; j <= val - i; j++)
-                {
-                    Console.Write(" ");
-                }
-                for(k = 1; k <= i; k++)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
-            }
+            //int val = 8;
+            //int i, j, k;
+            //for(i = 1; i <=val; i++)
+            //{
+            //    for (j = 1; j <= val - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for(k = 1; k <= i; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
 
-            // Pattern 4
+            //// Pattern 4
 
-            int num = 8;
+            //int num = 8;
 
-            for(i = 0; i < num; ++i)
-            {
-                for(j = 0; j<=i; ++j)
-                {
-                    Console.Write("*");
-                }
-                if(i != num - 1)
-                {
-                    Console.Write(" ");
-                }
-                else
-                {
-                    Console.Write(" * ");
-                }
-                for(j=0; j <= i; ++j)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
-            }
+            //for(i = 0; i < num; ++i)
+            //{
+            //    for(j = 0; j<=i; ++j)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    if(i != num - 1)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    else
+            //    {
+            //        Console.Write(" * ");
+            //    }
+            //    for(j=0; j <= i; ++j)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
 
-            // Pattern 5
-            int number = 7;
+            //// Pattern 5
+            //int number = 7;
 
-            for(i = 0; i < number; i++)
-            {
-                if(i == 0 || i == 6)
-                {
-                    for(j=0; j < number; j++)
-                    {
-                        Console.Write("*");
-                    }
-                    Console.WriteLine();
-                }
-                if(i>=1 && i <= 5)
-                {
-                    for(j=0; j< number; j++)
-                    {
-                        if(j == 0 || j == 6)
-                        {
-                            Console.Write("*");
-                        }
-                        else if(j >= 1 && j <= 5)
-                        {
-                            Console.Write(" ");
-                        }
-                    }
-                    Console.WriteLine();
-                }
-            }
+            //for(i = 0; i < number; i++)
+            //{
+            //    if(i == 0 || i == 6)
+            //    {
+            //        for(j=0; j < number; j++)
+            //        {
+            //            Console.Write("*");
+            //        }
+            //        Console.WriteLine();
+            //    }
+            //    if(i>=1 && i <= 5)
+            //    {
+            //        for(j=0; j< number; j++)
+            //        {
+            //            if(j == 0 || j == 6)
+            //            {
+            //                Console.Write("*");
+            //            }
+            //            else if(j >= 1 && j <= 5)
+            //            {
+            //                Console.Write(" ");
+            //            }
+            //        }
+            //        Console.WriteLine();
+            //    }
+            //}
 
             // Pattern 6
+
+            int numb, i, k, count = 1;
+            Console.WriteLine("Enter the number of rows: ");
+            numb = int.Parse(Console.ReadLine());
+
+            
+
+
 
 
             // Pattern 7
