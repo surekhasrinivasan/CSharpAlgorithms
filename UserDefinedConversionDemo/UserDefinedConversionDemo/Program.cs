0c06a1d74bfae1bd21aa3e356562e2fcb700f1a4
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-21 21:16:46 -0400

    UserDefinedConversionDemo - initial commit

---
+++ UserDefinedConversionDemo/UserDefinedConversionDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDefinedConversionDemo
+{
+    // Program to demo User Defined Conversion
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
