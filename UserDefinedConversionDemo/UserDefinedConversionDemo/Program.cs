3922c4105c08df52eea6f7cb6af6ea972a1b653e
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-21 21:28:36 -0400

    Declare class Person and its constructor

--- UserDefinedConversionDemo/UserDefinedConversionDemo/Program.cs
+++ UserDefinedConversionDemo/UserDefinedConversionDemo/Program.cs
@@ -7,6 +7,17 @@ using System.Threading.Tasks;
 namespace UserDefinedConversionDemo
 {
     // Program to demo User Defined Conversion
+
+    class Person
+    {
+        public string Name;
+        public int Age;
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
