a9bf7a07fbd654ab1bd5d3474b170ec2044ec8c3
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-22 23:05:51 -0400

    declare KnapSack method

--- KnapsackAlgorithm/KnapsackAlgorithm/Program.cs
+++ KnapsackAlgorithm/KnapsackAlgorithm/Program.cs
@@ -9,6 +9,11 @@ namespace KnapsackAlgorithm
     // Program to implement knapsack algorithm
     class Program
     {
+        public static int KnapSack(int capacity, int[] weight, int[] value, int itemsCount)
+        {
+
+        }
+
         static void Main(string[] args)
         {
         }
