6d7805d9578b96cd3c728f7da03a0640140efd34
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-23 22:13:07 -0400

    BitcountAlgorithm - initial commit

---
+++ BitcountAlgorithm/BitcountAlgorithm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitcountAlgorithm
+{
+    // Program to demo Sparsh bitcount algorithm
+
+    // Bitcount is useful when using compact data structures in memory with bits. Sparsh bitcount is a simple and 
+    // fast algorithm that walks through all the bits that are set to one. It is static. It does not rely on saving state. 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
