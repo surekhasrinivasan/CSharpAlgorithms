c8cc8e59cd5fa17fbc21700de54d0aa7aba92831
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-23 21:56:46 -0400

    Implementation of KnapSack method

--- KnapsackAlgorithm/KnapsackAlgorithm/Program.cs
+++ KnapsackAlgorithm/KnapsackAlgorithm/Program.cs
@@ -11,7 +11,21 @@ namespace KnapsackAlgorithm
     {
         public static int KnapSack(int capacity, int[] weight, int[] value, int itemsCount)
         {
+            int[,] k = new int[itemsCount + 1, capacity + 1];
 
+            for(int i=0; i <= itemsCount; ++i)
+            {
+                for(int w =0; w <= capacity; ++w)
+                {
+                    if (i == 0 || w == 0)
+                        k[i, w] = 0;
+                    else if (weight[i - 1] <= w)
+                        k[i, w] = Math.Max(value[i - 1] + k[i - 1, w - weight[i - 1]], k[i - 1, w]);
+                    else
+                        k[i, w] = k[i - 1, w];                
+                }
+            }
+            return k[itemsCount, capacity];
         }
 
         // main method
