9e8ab5de47782c441971208a45e486b455ccb85d
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-26 22:05:40 -0400

    MaxOccurrenceAlgorithm - initial commit

---
+++ MaxOccurrenceAlgorithm/MaxOccurrenceAlgorithm/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxOccurrenceAlgorithm
+{
+    // Program to find the most frequent element in an Array
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
