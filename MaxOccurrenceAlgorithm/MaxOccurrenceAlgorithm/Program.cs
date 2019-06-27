e39c03114ce2b247826355e5330be5c8adde2bb9
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-26 22:07:37 -0400

    declare MaxOccurrence method

--- MaxOccurrenceAlgorithm/MaxOccurrenceAlgorithm/Program.cs
+++ MaxOccurrenceAlgorithm/MaxOccurrenceAlgorithm/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,10 @@ namespace MaxOccurrenceAlgorithm
     // Program to find the most frequent element in an Array
     class Program
     {
+        static void MaxOccurrence(int[] array, Hashtable hs)
+        {
+
+        }
         static void Main(string[] args)
         {
         }
