e6991e9316466011894a445c0a4aa5c2c5d2dfb1
Surekhasrinivasan <surekhasrao@gmail.com>
2019-06-29 23:04:40 -0400

    PrintMultiplicationTable - initial commit

---
+++ PrintMultiplicationTable/PrintMultiplicationTable/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintMultiplicationTable
+{
+    // Program to print multiplication table
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
