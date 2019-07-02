7a225278295898f1d3f8fb6fa88b8d3cb63d504f
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-01 21:41:35 -0400

    PowerFunctionAlgorithm - initial commit

---
+++ PowerFunctionAlgorithm/PowerFunctionAlgorithm/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerFunctionAlgorithm
+{
+    // Program that demo power function()
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
