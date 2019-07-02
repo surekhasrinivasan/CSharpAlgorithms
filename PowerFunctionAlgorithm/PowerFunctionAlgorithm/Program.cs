ff73db059c66dde437d6c04c07893b152fed397e
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-01 21:47:14 -0400

    Take the user input for base and exponent

--- PowerFunctionAlgorithm/PowerFunctionAlgorithm/Program.cs
+++ PowerFunctionAlgorithm/PowerFunctionAlgorithm/Program.cs
@@ -7,10 +7,20 @@ using System.Threading.Tasks;
 namespace PowerFunctionAlgorithm
 {
     // Program that demo power function()
+
+    // The function takes 2 parameters - Base and Exponent. The function returns back the result of "Base raised to the power of exponent". 
+    // For example - When the Power() method is called using parameters 3 and 4.The value of 3 to the power of 4 should be returned back, i.e 81.
     class Program
     {
         static void Main(string[] args)
         {
+            // Prompt the user to enter base
+            Console.WriteLine("Enter your base number: ");
+            int Base = Convert.ToInt32(Console.ReadLine());
+
+            // Prompt the user to enter exponent
+            int Exponent = Convert.ToInt32(Console.ReadLine());
+
         }
     }
 }
