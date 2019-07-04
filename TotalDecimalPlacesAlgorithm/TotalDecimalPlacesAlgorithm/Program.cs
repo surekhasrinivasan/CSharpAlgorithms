fc0e94d6bbd043b98a8d8bdc40bcac40c32a2bb2
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-04 18:24:54 -0400

    TotalDecimalPlacesAlgorithm - initial commit

---
+++ TotalDecimalPlacesAlgorithm/TotalDecimalPlacesAlgorithm/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalDecimalPlacesAlgorithm
+{
+    // Program to get the total number of decimal places. Example 
+    // Input : 1, 1.0, 1.1, 1.12
+    // Output : 0, 0, 1, 2
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
