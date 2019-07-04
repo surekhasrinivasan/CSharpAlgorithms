560933cf7a4097991fe62da82fe3004abc219191
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-04 18:36:39 -0400

    Implementation of GetDecimalPartCount() method

--- TotalDecimalPlacesAlgorithm/TotalDecimalPlacesAlgorithm/Program.cs
+++ TotalDecimalPlacesAlgorithm/TotalDecimalPlacesAlgorithm/Program.cs
@@ -17,9 +17,22 @@ namespace TotalDecimalPlacesAlgorithm
         }
 
         // Function that return the total decimal places
-        private static int GetDecimalPartCount()
+        private static int GetDecimalPartCount(decimal decimalNumber)
         {
+            // Dividing decimal number with 1 gives the decimal part
+            decimal decimalPlaces = decimalNumber % 1;
+            if(decimalPlaces != 0)
+            {
+                // Get the index of dot from the decimal part
+                int indexOfDot = decimalPlaces.ToString().IndexOf('.');
 
+                // Use the 0.######## format string to rip off trailing zeros, and get the count 
+                int numberOfDecimals = decimalPlaces.ToString("0.##########").Substring(indexOfDot).Length - 1;
+
+                return numberOfDecimals;
+            }
+            // Finally convert decimal to int and return
+            return (int)decimalPlaces;
         }
     }
 }
