a245197e0f8bb783c783c6883ed314facf2eb915
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-03 21:44:24 -0400

    SpaceBeforeUpperCaseLetter - initial commit

---
+++ SpaceBeforeUpperCaseLetter/SpaceBeforeUpperCaseLetter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceBeforeUpperCaseLetter
+{
+    // Program that inserts a single space before every upper case letter. For example, 
+    // if we have string like "ProductUnitPrice", the program should convert it to "Product Unit Price".
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
