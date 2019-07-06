d9ffd6301a0cabeff733a3f2076127b930dfb24a
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-05 22:37:12 -0400

    RemoveTrailingZerosAlgorithm - completed

--- RemoveTrailingZerosAlgorithm/RemoveTrailingZerosAlgorithm/Program.cs
+++ RemoveTrailingZerosAlgorithm/RemoveTrailingZerosAlgorithm/Program.cs
@@ -12,6 +12,12 @@ namespace RemoveTrailingZerosAlgorithm
         static void Main(string[] args)
         {
             decimal[] decimalNumbers = { 1.0M, 1.01M, 1.0010M, 0.00M, 1.0050M};
+
+            foreach(decimal decimalNumber in decimalNumbers)
+            {
+                Console.WriteLine("Original Decimal Number = {0}, Without Zeros = {1}",
+                                    decimalNumber, decimalNumber.ToString("0.####"));
+            }
         }
     }
 }
