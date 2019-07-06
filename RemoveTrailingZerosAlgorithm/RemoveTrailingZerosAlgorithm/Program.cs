8b37a2b30fac76c3d7f2e77ebbcd6c4a935c1417
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-05 22:11:27 -0400

    RemoveTrailingZerosAlgorithm - initial commit

---
+++ RemoveTrailingZerosAlgorithm/RemoveTrailingZerosAlgorithm/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveTrailingZerosAlgorithm
+{
+    // Program to remove trailing zeros in a decimal
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
