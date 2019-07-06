8239786ddca5f71184b4d167da3cc3ba7940b5bc
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-06 16:13:17 -0400

    FindSmallestAndLargestNum - completed

--- FindSmallestAndLargestNum/FindSmallestAndLargestNum/Program.cs
+++ FindSmallestAndLargestNum/FindSmallestAndLargestNum/Program.cs
@@ -14,7 +14,17 @@ namespace FindSmallestAndLargestNum
             // Declare and initialize the integer array
             int[] NumbersArray = { 102, 34, 89, 12, 187, 29, 111 };
 
+            // Sort the array, first element in the array will be 
+            // smallest and the last element will be largest
+            Array.Sort(NumbersArray);
 
+            // Print the smallest number in the array
+            Console.WriteLine("Smallest Number = {0}", NumbersArray[0]);
+
+            // Print the largest number in the array
+            Console.WriteLine("Largest Number = {0}", NumbersArray[NumbersArray.Length-1]);
+
+            Console.ReadLine();
         }
     }
 }
