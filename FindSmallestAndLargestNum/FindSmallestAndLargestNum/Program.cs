fe973c5f37071789854b660967afa67a99d2033d
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-06 16:16:52 -0400

    FindSmallestAndLargestNum - completed using Linq extension methods Min() and Max()

--- FindSmallestAndLargestNum/FindSmallestAndLargestNum/Program.cs
+++ FindSmallestAndLargestNum/FindSmallestAndLargestNum/Program.cs
@@ -14,15 +14,21 @@ namespace FindSmallestAndLargestNum
             // Declare and initialize the integer array
             int[] NumbersArray = { 102, 34, 89, 12, 187, 29, 111 };
 
-            // Sort the array, first element in the array will be 
-            // smallest and the last element will be largest
-            Array.Sort(NumbersArray);
+            //// Sort the array, first element in the array will be 
+            //// smallest and the last element will be largest
+            //Array.Sort(NumbersArray);
 
-            // Print the smallest number in the array
-            Console.WriteLine("Smallest Number = {0}", NumbersArray[0]);
+            //// Print the smallest number in the array
+            //Console.WriteLine("Smallest Number = {0}", NumbersArray[0]);
 
-            // Print the largest number in the array
-            Console.WriteLine("Largest Number = {0}", NumbersArray[NumbersArray.Length-1]);
+            //// Print the largest number in the array
+            //Console.WriteLine("Largest Number = {0}", NumbersArray[NumbersArray.Length-1]);
+
+            //Console.ReadLine();
+
+            // Method II using Linq extension methods Min() and Max()
+            Console.WriteLine("Smallest Number = {0}", NumbersArray.Min());
+            Console.WriteLine("Largest Number = {0}", NumbersArray.Max());
 
             Console.ReadLine();
         }
