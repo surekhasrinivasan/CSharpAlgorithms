9ba40c4f1cf6054c363f4fe8191539e412619fa4
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-06 16:03:15 -0400

    FindSmallestAndLargestNum - initial commit

---
+++ FindSmallestAndLargestNum/FindSmallestAndLargestNum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindSmallestAndLargestNum
+{
+    // Program to find and print, the smallest and largest number in the integer array
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
