05602e5430f0da5091939a24998bfa028081ab83
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-08 17:41:49 -0400

    CountEmailsByDomain - initial commit

---
+++ CountEmailsByDomain/CountEmailsByDomain/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountEmailsByDomain
+{
+    // Program that lists the total number of emails by domain
+    // Ex: Input : "aa@xyz.com;cc@abc.com;bb@abc.com;dd@abc.com";
+    //    Output : Domain = xyz.com & Count = 1, Domain = abc.com & Count = 3
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
