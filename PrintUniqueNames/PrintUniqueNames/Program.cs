4b602e43ec85051a13ccaef5c1e11aa2f11fc380
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-07 21:54:13 -0400

    Take User input

--- PrintUniqueNames/PrintUniqueNames/Program.cs
+++ PrintUniqueNames/PrintUniqueNames/Program.cs
@@ -13,6 +13,11 @@ namespace PrintUniqueNames
     {
         static void Main(string[] args)
         {
+            // Prompt the user to enter the list of user names
+            Console.WriteLine("Enter a list of names separated by semicolon: ");
+
+            // Read the user name list from the console
+            string strUserNames = Console.ReadLine();
         }
     }
 }
