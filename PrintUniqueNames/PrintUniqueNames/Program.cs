7594d9dcd7cd9da42b02c35ab74142868d2f1355
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-07 21:50:45 -0400

    PrintUniqueNames - initial commit

---
+++ PrintUniqueNames/PrintUniqueNames/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintUniqueNames
+{
+    // Program to print unique names, by removing duplicate entries. 
+    // example: Input - "Rob;Mike;Able;Sara;Rob;Peter;Able;"
+    //          Output - "Rob;Mike;Able;Sara;Peter;"
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
