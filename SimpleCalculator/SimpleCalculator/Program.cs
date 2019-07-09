377c4c857db2e376bd4b430be7919fbc36da3a07
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-09 17:26:05 -0400

    SimpleCalculator - initial commit

---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    // Program that builds a simple calculator which has following features:
+    // Adding 2 Numbers
+    // Subtracting 2 Numbers
+    // Multiplying 2 Numbers
+    // Dividing 2 Numbers
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
