b631feb521a24ca452a7c0a0466fdc190ac59e02
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-30 21:51:46 -0400

    implementation of PrintMenu() method

--- SimpleCalculator/SimpleCalculator/Program.cs
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -18,9 +18,16 @@ namespace SimpleCalculator
         {
 
         }
-        public void PrintMenu()
+        public static void PrintMenu()
         {
-
+            Console.WriteLine();
+            Console.WriteLine("Select your option");
+            Console.WriteLine("------------------");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.WriteLine();
         }
 
         public static void Calculate(int SelectedOption)
