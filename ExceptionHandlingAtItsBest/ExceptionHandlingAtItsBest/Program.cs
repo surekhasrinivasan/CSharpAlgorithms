467b1044c7736786fe25f983237609e12ec6cb62
Surekhasrinivasan <surekhasrao@gmail.com>
2019-07-31 21:21:26 -0400

    ExceptionHandlingAtItsBest - initial commit

---
+++ ExceptionHandlingAtItsBest/ExceptionHandlingAtItsBest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingAtItsBest
+{
+// Write a c# program to add two numbers. This may sound very simple, but the catch, is - The program should not break, and always should give meaningful error messages when exception conditions occur. For example, the following error conditions should be handled in your program. Also, the program should run as long as the user wants it to run.
+// 1. If the user enters "Ten" instead of 10, the program should let the user know only numbers can be added.
+// 2. If the user, enters a very large number, the program should let the user know about the range allowed.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
