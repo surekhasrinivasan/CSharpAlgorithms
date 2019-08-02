31e7a76cd14b95e0ba048e8118d910d9d299e64a
Surekhasrinivasan <surekhasrao@gmail.com>
2019-08-01 21:41:43 -0400

    ExplicitInterfaceImplementation - initial commit

---
+++ ExplicitInterfaceImplementation/ExplicitInterfaceImplementation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterfaceImplementation
+{
+    // Program to demo use of Explicit Interface implementation technique to implement void Method
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
