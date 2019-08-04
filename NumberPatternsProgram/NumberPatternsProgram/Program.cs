6c7fbfade11150a7b2e3a30b44f8052a95dfd130
Surekhasrinivasan <surekhasrao@gmail.com>
2019-08-03 22:15:52 -0400

    Pyramid number pattern - completed

--- NumberPatternsProgram/NumberPatternsProgram/Program.cs
+++ NumberPatternsProgram/NumberPatternsProgram/Program.cs
@@ -20,9 +20,24 @@ namespace NumberPatternsProgram
 
                 num = Convert.ToInt32(Console.ReadLine());
 
-                space = num - 1;
-            
+                space = num - 1; 
 
+                for(int i = 1; i <= num; i++)
+                {
+                    for(space = 1; space <=(num-i); space++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for(int j=1; j <= i; j++)
+                    {
+                        Console.Write(j);
+                    }
+                    for(int k = i - 1; k >= 1; k--)
+                    {
+                        Console.Write(k);
+                    }
+                    Console.WriteLine();
+                }
             }
         }
     }
