e9217d94577a5df302d9c52fe20b67b27e976c2a
Surekhasrinivasan <surekhasrao@gmail.com>
2019-08-05 21:34:03 -0400

    Number Pattern - 4 - initial commit

--- NumberPatternsProgram/NumberPatternsProgram/Program.cs
+++ NumberPatternsProgram/NumberPatternsProgram/Program.cs
@@ -57,31 +57,37 @@ namespace NumberPatternsProgram
         //    Console.ReadKey();
         //}
 
-        // Number pattern 3
+        //// Number pattern 3
+        //public static void Main()
+        //{
+        //    Console.WriteLine("Enter a number: ");
+        //    int num = Convert.ToInt32(Console.ReadLine());
+
+        //    Console.WriteLine();
+
+        //    for(int i = 1; i < num; i++)
+        //    {
+        //        for(int j = 1; j <= i; j++)
+        //        {
+        //            Console.Write(j.ToString());                    
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //    for(int i = num; i >=0; i--)
+        //    {
+        //        for(int j = 1; j <= i; j++)
+        //        {
+        //            Console.Write(j.ToString());                    
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //    Console.WriteLine();
+        //}
+
+        // Number pattern 4
         public static void Main()
         {
-            Console.WriteLine("Enter a number: ");
-            int num = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine();
-
-            for(int i = 1; i < num; i++)
-            {
-                for(int j = 1; j <= i; j++)
-                {
-                    Console.Write(j.ToString());                    
-                }
-                Console.WriteLine();
-            }
-            for(int i = num; i >=0; i--)
-            {
-                for(int j = 1; j <= i; j++)
-                {
-                    Console.Write(j.ToString());                    
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
         }
     }
 }
