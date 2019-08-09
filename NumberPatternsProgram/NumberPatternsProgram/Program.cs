5a4c9bd639fd65f94efc73457d3dbbedfa17bc2c
Surekhasrinivasan <surekhasrao@gmail.com>
2019-08-08 21:24:12 -0400

    Number Pattern 6 - completed

--- NumberPatternsProgram/NumberPatternsProgram/Program.cs
+++ NumberPatternsProgram/NumberPatternsProgram/Program.cs
@@ -153,6 +153,12 @@ namespace NumberPatternsProgram
 
             Console.Write("Input number of rows : ");
             rows = Convert.ToInt32(Console.ReadLine());
+            for(i = 1; i <= rows; i++)
+            {
+                for(j = 1; j <= i; j++)
+                    Console.Write("{0} ", k++);
+                Console.Write("\n");
+            }
         }
     }
 }
