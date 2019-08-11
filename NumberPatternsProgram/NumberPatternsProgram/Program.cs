31d763a7f55b21341d882651487979dabd9a014f
Surekhasrinivasan <surekhasrao@gmail.com>
2019-08-10 21:31:28 -0400

    Number pattern - 8 completed

--- NumberPatternsProgram/NumberPatternsProgram/Program.cs
+++ NumberPatternsProgram/NumberPatternsProgram/Program.cs
@@ -189,7 +189,19 @@ namespace NumberPatternsProgram
             Console.Write("Enter the no. of row you want to print: ");
             row = Convert.ToInt32(Console.ReadLine());
 
-
+            for(i = 0; i  <= row; i++)
+            {
+                k = 1;
+                for (j = i; j <= row - 1; j++)
+                    Console.Write(" ");
+                
+                for (j = 0; j <= i; j++)
+                {
+                    Console.Write("{0} ", k);
+                    k = (k * (i - j) / (j + 1));
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
