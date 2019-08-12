8af18e1fcf48b21072915862435c4d4ab1939af0
Surekhasrinivasan <surekhasrao@gmail.com>
2019-08-11 21:09:24 -0400

    Number pattern -9 initial commit

--- NumberPatternsProgram/NumberPatternsProgram/Program.cs
+++ NumberPatternsProgram/NumberPatternsProgram/Program.cs
@@ -182,27 +182,34 @@ namespace NumberPatternsProgram
         //    }
         //}
 
-        // Number pattern 8
+        //// Number pattern 8
+        //public static void Main()
+        //{
+        //    int row, i, j, k;
+        //    Console.Write("Enter the no. of row you want to print: ");
+        //    row = Convert.ToInt32(Console.ReadLine());
+
+        //    for(i = 0; i  <= row; i++)
+        //    {
+        //        k = 1;
+        //        for (j = i; j <= row - 1; j++)
+        //            Console.Write(" ");
+                
+        //        for (j = 0; j <= i; j++)
+        //        {
+        //            Console.Write("{0} ", k);
+        //            k = (k * (i - j) / (j + 1));
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //    Console.ReadLine();
+        //}
+
+        // Number pattern 9
         public static void Main()
         {
-            int row, i, j, k;
-            Console.Write("Enter the no. of row you want to print: ");
-            row = Convert.ToInt32(Console.ReadLine());
 
-            for(i = 0; i  <= row; i++)
-            {
-                k = 1;
-                for (j = i; j <= row - 1; j++)
-                    Console.Write(" ");
-                
-                for (j = 0; j <= i; j++)
-                {
-                    Console.Write("{0} ", k);
-                    k = (k * (i - j) / (j + 1));
-                }
-                Console.WriteLine();
-            }
-            Console.ReadLine();
         }
     }
 }
+
