42a01c1c0045b9ef55707c692d33a2e596adde15
Surekhasrinivasan <surekhasrao@gmail.com>
2019-08-16 17:03:29 -0400

    LevenshteinDistanceAlgorithm - initial commit

---
+++ LevenshteinDistanceAlgorithm/LevenshteinDistanceAlgorithm/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevenshteinDistanceAlgorithm
+{
+    // Program to demo Levenshtein Distance Algorithm
+
+    // The Levenshtein distance is a string metric for measuring the difference between two sequences. 
+    // The Levenshtein distance between two words is the minimum number of single-character edits 
+    // (i.e. insertions, deletions or substitutions) required to change one word into the other.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
