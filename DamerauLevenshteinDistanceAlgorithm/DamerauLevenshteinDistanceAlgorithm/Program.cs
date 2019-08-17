a114be5c8aa32c4044ac264b50a012686005666e
Surekhasrinivasan <surekhasrao@gmail.com>
2019-08-17 19:10:16 -0400

    DamerauLevenshteinDistanceAlgorithm - initial commit

---
+++ DamerauLevenshteinDistanceAlgorithm/DamerauLevenshteinDistanceAlgorithm/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamerauLevenshteinDistanceAlgorithm
+{
+    // Program to demo Damerau Levenshtein Distance Algorithm
+
+    // The Damerau–Levenshtein distance differs from the classical Levenshtein distance by including transpositions among its allowable operations. 
+    // The classical Levenshtein distance only allows insertion, deletion, and substitution operations.Modifying this distance by including 
+    // transpositions of adjacent symbols produces a different distance measure, known as the Damerau–Levenshtein distance.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
