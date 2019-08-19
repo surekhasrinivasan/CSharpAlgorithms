9a6d277cfa396a545193ac00119c5b955395b055
Surekhasrinivasan <surekhasrao@gmail.com>
2019-08-19 17:30:49 -0400

    CreateGraph method created and implemented

--- BellmanFordAlgorithm/BellmanFordAlgorithm/Program.cs
+++ BellmanFordAlgorithm/BellmanFordAlgorithm/Program.cs
@@ -25,6 +25,16 @@ namespace BellmanFordAlgorithm
             public Edge[] edge;
         }
 
+        public static Graph CreateGraph(int verticesCount, int edgesCount)
+        {
+            Graph graph = new Graph();
+            graph.VerticesCount = verticesCount;
+            graph.EdgesCount = edgesCount;
+            graph.edge = new Edge[graph.EdgesCount];
+
+            return graph;
+        }
+
         static void Main(string[] args)
         {
         }
