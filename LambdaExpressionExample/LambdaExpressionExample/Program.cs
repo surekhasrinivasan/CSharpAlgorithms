0cd26ca64847c1ebca22af55c2cd438c9ea455e0
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-01 21:30:03 -0400

    Program to modify local variable using lambda expression

--- LambdaExpressionExample/LambdaExpressionExample/Program.cs
+++ LambdaExpressionExample/LambdaExpressionExample/Program.cs
@@ -6,29 +6,32 @@ using System.Threading.Tasks;
 
 namespace LambdaExpressionExample
 {
-    // Program to demo Lambda Expression
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            DoWork(6);
-            Console.ReadLine();
-        }
+    //// Program to demo Lambda Expression
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        DoWork(6);
+    //        Console.ReadLine();
+    //    }
 
-        public static void DoWork(int intnumber1)
-        {
-            int intnumber2 = 2;
+    //    public static void DoWork(int intnumber1)
+    //    {
+    //        int intnumber2 = 2;
 
-            Action<int> action = (num) =>
-            {
-                for (int i = num; i < num + (intnumber1 * intnumber2); i++)
-                {
-                    int result = i * 3 - 1;
-                    Console.WriteLine(string.Format("{0}, {1}", i, result));
+    //        Action<int> action = (num) =>
+    //        {
+    //            for (int i = num; i < num + (intnumber1 * intnumber2); i++)
+    //            {
+    //                int result = i * 3 - 1;
+    //                Console.WriteLine(string.Format("{0}, {1}", i, result));
 
-                }
-            };
-            action(9);
-        }
-    }
+    //            }
+    //        };
+    //        action(9);
+    //    }
+    //}
+
+    // Program to modify local variables using Lambda Expression
+
 }
