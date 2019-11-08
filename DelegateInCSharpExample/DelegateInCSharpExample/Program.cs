d4dad91563dcf69562b2437c7f4bddd1b2c88586
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-07 19:05:07 -0500

    Program to demo Delegate Example

---
+++ DelegateInCSharpExample/DelegateInCSharpExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Program to demo Delegate Example in CSharp
+namespace DelegateInCSharpExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
